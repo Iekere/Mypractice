ffdfdbe4ee5c13099d9ef4da7a4f5e051da6505e
iekere <innocentekere28@gmail.com>
2025-06-20 09:03:37 +0100

    familyName

---
+++ familyName/main.go
@@ -0,0 +1,13 @@
+package main
+import "fmt"
+func familyName(fName string) {
+fmt.Println("Hello", fName, "Ekere")
+}
+
+func main() {
+familyName("Innocent")
+familyName("patience")
+familyName("Gloria")
+familyName("veronica")
+familyName("Esther")
+}
